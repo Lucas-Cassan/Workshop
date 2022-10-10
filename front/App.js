69ac7b369fee27f28331f3481478f31da5c38193
lolabaran1 <lola@coderkaine.com>
2022-10-10 16:06:56 +0200

    src folder

--- front/App.js
+++ front/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Appbar from './src/components/Appbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <Appbar/>
     </View>
   );
 }
@@ -16,5 +16,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: 'auto',
   },
 });
