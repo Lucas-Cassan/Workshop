f03cc6fac78d546b5290806cb0cca93066fbc525
pierrebec <pierre.bec@gmail.com>
2022-10-10 14:01:16 +0200

    init api

--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
 const port = 80;
 
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const uri = process.env.MONGODB;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+    app.locals.client = client;
+});
+
+const userRoutes = require('./routes/user');
+app.use('/user', userRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
