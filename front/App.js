2f437be5380ae321ec3115931fe784547502590c
Lucas-Cassan <lucascassan@outlook.com>
2022-10-11 14:35:37 +0200

    fit: add component font

--- front/App.js
+++ front/App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Appbar from './src/components/Appbar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
-import StackNavigator from './StackNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Appbar from "./src/components/Appbar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button } from "react-native";
+import StackNavigator from "./StackNavigator";
+import Fonts from "./src/components/Fonts";
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <StackNavigator/>
-        <StatusBar style='dark'/>
-        <Appbar/>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Fonts />
+      <StackNavigator />
+      <StatusBar style="dark" />
+      <Appbar />
+    </NavigationContainer>
   );
 }
