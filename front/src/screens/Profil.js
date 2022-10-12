3fed8ca98f8817c476bb8adea056dbdcd847f10c
lolabaran1 <lola@coderkaine.com>
2022-10-12 11:30:14 +0200

    connection

--- front/src/screens/Profil.js
+++ front/src/screens/Profil.js
@@ -1,14 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaView, Text, Button } from "react-native";
 import Toppage from "../components/Toppage";
 import styles from "./style";
 
 const Profil = () => {
-    return (
-      <SafeAreaView style={styles.view}>
-        <Toppage subTitle={"Bienvenue sur ton profil"}>Profil</Toppage>
-      </SafeAreaView>
-    );
-  };
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView style={styles.view}>
+      <Toppage subTitle={"Bienvenue sur ton profil"}>Profil</Toppage>
+      <Text onPress={() => navigation.navigate("Auth")}>Se connecter</Text>
+    </SafeAreaView>
+  );
+};
 
 export default Profil;
