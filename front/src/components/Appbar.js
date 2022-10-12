67decfa0e4c65c61ebe45d3ba1fe325af95ad1be
Lucas-Cassan <lucascassan@outlook.com>
2022-10-12 10:04:35 +0200

    fix: bottom bar

--- front/src/components/Appbar.js
+++ front/src/components/Appbar.js
@@ -9,10 +9,7 @@ import {
   Image,
 } from "@react-native-material/core";
 import { useNavigation } from "@react-navigation/native";
-import { Ionicons } from "@expo/vector-icons";
-import { createIconSetFromFontello } from "react-native-vector-icons";
-import fontelloConfig from "../../src/config.json";
-const Icon = createIconSetFromFontello(fontelloConfig);
+import { FontAwesome5 } from "@expo/vector-icons";
 
 const Appbar = () => {
   const navigation = useNavigation();
@@ -23,35 +20,33 @@ const Appbar = () => {
       variant="bottom"
       color="#ffffff"
       tintColor="#98D7F6"
-      leading={(props) => (
-        <IconButton
-          icon={(props) => <Icon name="home" {...props} />}
-          {...props}
-          color={"#98D7F6"}
-          onPress={() => navigation.navigate("Home")}
-        />
-      )}
-      leadingContainerStyle={{ margin: 0, padding: 0 }}
     >
       <IconButton
         style={styles.button}
         color="#98D7F6"
+        onPress={() => navigation.navigate("Home")}
+        icon={(props) => <FontAwesome5 name="home" {...props} />}
+        variant={"text"}
+      />
+      <IconButton
+        style={styles.button}
+        color="#98D7F6"
         onPress={() => navigation.navigate("Feedback")}
-        icon={(props) => <Icon name="feedback" {...props} />}
+        icon={(props) => <FontAwesome5 name="clipboard-check" {...props} />}
         variant={"text"}
       />
       <IconButton
         style={styles.button}
         color="#98D7F6"
         onPress={() => navigation.navigate("Events")}
-        icon={(props) => <Ionicons name="ios-calendar" {...props} />}
+        icon={(props) => <FontAwesome5 name="calendar-week" {...props} />}
         variant={"text"}
       />
       <IconButton
         style={styles.button}
         color="#98D7F6"
         onPress={() => navigation.navigate("Profil")}
-        icon={(props) => <Ionicons name="ios-person" {...props} />}
+        icon={(props) => <FontAwesome5 name="trophy" {...props} />}
         variant={"text"}
       />
     </AppBar>
@@ -63,8 +58,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-evenly",
-    width: "100%",
+    justifyContent: "space-around",
     position: "absolute",
     start: 0,
     end: 0,
@@ -76,9 +70,7 @@ const styles = StyleSheet.create({
     marginRight: "7.5%",
     marginBottom: "4%",
   },
-  button: {
-    marginRight: 20,
-  },
+  button: {},
 });
 
 export default Appbar;
